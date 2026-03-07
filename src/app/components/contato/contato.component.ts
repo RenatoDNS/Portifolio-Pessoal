1d4b52e134234bb344002c015ce27b1fa5a29b6d
RenatoDNS <renatodouglasnso@gmail.com>
2026-03-06 23:53:04 -0300

    feat: add email api

--- src/app/components/contato/contato.component.ts
+++ src/app/components/contato/contato.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, AfterViewInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { heroPaperAirplaneSolid } from '@ng-icons/heroicons/solid';
 import { TranslationService } from '../../services/translation.service';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contato',
@@ -11,7 +12,7 @@ import { TranslationService } from '../../services/translation.service';
   imports: [FormsModule, NgIcon],
   providers: [provideIcons({ heroPaperAirplaneSolid })],
 })
-export class ContatoComponent {
+export class ContatoComponent implements AfterViewInit {
   tSer = inject(TranslationService);
 
   contato = {
@@ -20,11 +21,52 @@ export class ContatoComponent {
     mensagem: '',
   };
 
+  isLoading = false;
+  successMessage = '';
+  errorMessage = '';
+
+  // Configurações do EmailJS - substitua com suas chaves
+  private readonly SERVICE_ID = 'YOUR_SERVICE_ID';
+  private readonly TEMPLATE_ID = 'YOUR_TEMPLATE_ID';
+  private readonly PUBLIC_KEY = 'YOUR_PUBLIC_KEY';
+
+  ngAfterViewInit() {
+    emailjs.init(this.PUBLIC_KEY);
+  }
+
   onSubmit(form: NgForm) {
     if (form.valid) {
-      console.log('Formulário enviado:', this.contato);
-      alert('Mensagem enviada com sucesso!');
-      form.resetForm();
+      this.isLoading = true;
+      this.errorMessage = '';
+      this.successMessage = '';
+
+      // Template no EmailJS tem que estar com os campos {{name}}, {{email}} e {{message}}
+      emailjs
+        .send(this.SERVICE_ID, this.TEMPLATE_ID, {
+          name: this.contato.nome,
+          email: this.contato.email,
+          message: this.contato.mensagem,
+        })
+        .then(
+          () => {
+            this.successMessage = 'Mensagem enviada com sucesso!';
+            form.resetForm();
+            this.contato = {
+              nome: '',
+              email: '',
+              mensagem: '',
+            };
+            this.isLoading = false;
+            setTimeout(() => {
+              this.successMessage = '';
+            }, 5000);
+          },
+          (error) => {
+            this.errorMessage = `Erro ao enviar mensagem: ${(error as EmailJSResponseStatus).text}`;
+            this.isLoading = false;
+            console.error('Erro ao enviar email:', error);
+          }
+        );
     }
   }
 }
